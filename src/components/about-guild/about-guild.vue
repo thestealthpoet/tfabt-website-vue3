c590d814e74489e9e83e177781da74f43f7ee1f6
Bryan McLain <bryan.r.mclain@gmail.com>
2025-07-21 19:33:08 -0400

    adding guild cozy image

--- src/components/about-guild/about-guild.vue
+++ src/components/about-guild/about-guild.vue
@@ -3,6 +3,14 @@
         <div class="about-text-wrapper">
             <h2>{{aboutTfabt}}</h2>
             <p class="guild-description">{{ guildDescription }}</p>
+            <p class="guild-extra-info">{{ extraInformation }}</p>
+        </div>
+        <div class="guild-cozy-image">
+            <img 
+            class="cozy-image"
+            alt="Cozy Guild Image"
+            src="../../assets/GuildCozyImage.png"
+            />
         </div>
     </div>
 </template>
@@ -14,7 +22,8 @@ export default {
     data() {
         return {
             aboutTfabt: "About TF&BT",
-            guildDescription: "TF&BT is a Christian and family-friendly guild focused on fellowship and playing together to succeed."
+            guildDescription: "TF&BT is a Christian and family-friendly guild focused on fellowship and playing together to succeed",
+            extraInformation: "We are located on the Dalaran server, happily accepting both Horde and Alliance players"
         }
     }
 }
@@ -25,6 +34,19 @@ export default {
 .about-guild-wrapper {
     display: flex;
     flex-direction: row;
+    gap: 5px;
+
+    .about-text-wrapper {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .guild-cozy-image {
+        .cozy-image {
+            width: 200px;
+            height: 200px;
+        }
+    }
 }
 
 
