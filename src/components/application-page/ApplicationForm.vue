fc86d06887b5f856239aa59347a9b37f57da65c9
Bryan McLain <bryan.r.mclain@gmail.com>
2025-08-04 01:07:09 -0400

    feat: iframe for guild application at navbar

---
+++ src/components/application-page/ApplicationForm.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="application-wrapper">
+        <iframe class="application-iframe" src="https://docs.google.com/forms/d/e/1FAIpQLSdD4K9geGi6xl5NzT7hn-W7YkZcQk5O340SRf52pr5a5xv8vg/viewform" />
+    </div>
+</template>
+
+<style scoped>
+.application-wrapper {
+    border-style: solid;
+    margin: 8px;
+
+    .application-iframe {
+        width: 100%;
+        height: 1000px;
+    }
+}
+
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../components/home-page/HomePage.vue'
 import Faqs from '../components/home-page/faqs/Faqs.vue'
+import ApplicationForm from '../components/application-page/ApplicationForm.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: HomePage },
-    { path: '/faqs', name: 'FAQ', component: Faqs }
+    { path: '/faqs', name: 'FAQ', component: Faqs },
+    { path: '/application', name: 'Application', component: ApplicationForm}
   ]
   
   export default createRouter({
